708671065849248b18ba61e808b939dc9b856809
Cainã Pimenta <caina.pimenta@alfaerp.com.br>
2026-01-26 09:43:55 -0300

    Vou falar nada

--- src/sql/purchaseInvoice/getPurchaseInvoicesByDraft.sql
+++ src/sql/purchaseInvoice/getPurchaseInvoicesByDraft.sql
@@ -3,7 +3,7 @@ SELECT
     T0."DocNum" AS "DocNum",
     T1."DocEntry" AS "DraftKey"
 FROM "{0}".OPCH T0
-    INNER JOIN ODRF T1 ON T1."DocEntry" = T0."draftKey"
+    INNER JOIN "{0}".ODRF T1 ON T1."DocEntry" = T0."draftKey"
 WHERE
     T0."CANCELED" = 'N'
     AND T1."DocEntry" = {1}
